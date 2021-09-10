2a496a344fe3817524ea05e19e274795b13108f0
Pedro Nazaré <pedro.nazare@pdcase.com.br>
2021-09-10 11:13:55 -0300

    initial commit

---
+++ Documents/Projetos/apiparametrosocupacao/src/test/java/com/pdcase/apiparametrosocupacao/ApiparametrosocupacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdcase.apiparametrosocupacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiparametrosocupacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
